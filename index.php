02e6710f15d1640ea77f9e6fcd8c52bbf12d7aae
anaestheticnerd-gif <anaestheticnerd@gmail.com>
2026-02-24 01:10:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,19 @@
 <?php
+$payload = file_get_contents("php://input");
 
-$bot_token = "8007417702:AAEwK_vxKpjKv797kr6UoRU1uFr7l0VpkG4";
+// نفس السر الموجود في InfinityFree
+$shared_secret = '12345';
 
-$content = file_get_contents("php://input");
-$update = json_decode($content, true);
+$ch = curl_init("https://khmiexam.gt.tc/api_bot_webhook_dispatch.php");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+  "Content-Type: application/json",
+  "X-Proxy-Secret: $shared_secret"
+]);
+curl_exec($ch);
+curl_close($ch);
 
-if(isset($update["message"])){
-
-    $chat_id = $update["message"]["chat"]["id"];
-    $text = $update["message"]["text"];
-
-    $url = "https://api.telegram.org/bot$bot_token/sendMessage";
-
-    $post_fields = [
-        'chat_id' => $chat_id,
-        'text' => "تم الاستلام: " . $text
-    ];
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_exec($ch);
-    curl_close($ch);
-}
+http_response_code(200);
+echo "OK";
